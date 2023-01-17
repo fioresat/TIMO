6db6189fea0d1c97dbfd7cb5e45778b52d86a2df
fioresat <mekhonoshinama@gmail.com>
2023-01-17 15:55:00 +0300

    quote_widget.dart some refactor

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        fontFamily: 'CeraPro'
+        fontFamily: 'CeraPro',
       ),
       home: const MyHomePage(),
     );
   }
 }
-
-
--- lib/ui/widgets/quote_widget.dart
+++ lib/ui/widgets/quote_widget.dart
@@ -53,80 +53,76 @@ class _QuoteWidgetState extends State<QuoteWidget> {
 
   Future _showDialog() => showGeneralDialog(
       context: context,
-      barrierDismissible: true,
-      barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
-      barrierColor: Colors.black45,
       transitionDuration: const Duration(milliseconds: 350),
       pageBuilder: (BuildContext buildContext, Animation animation,
           Animation secondaryAnimation) {
         double widthScreen = MediaQuery.of(context).size.width;
         double heightScreen = MediaQuery.of(context).size.height;
         return BackdropFilter(
-          filter: ui.ImageFilter.blur(sigmaX: 7.0, sigmaY: 7.0),
+          filter: ui.ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
           child: Container(
-              padding: EdgeInsets.only(
-                top: 0.03 * heightScreen,
-              ),
-              width: widthScreen,
-              height: heightScreen,
-              color: Colors.white.withOpacity(0.7),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          padding: EdgeInsets.only(
-                            top: 0.03 * heightScreen,
-                          ),
-                          backgroundColor: Colors.transparent,
-                          shadowColor: Colors.transparent),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Image.asset(
-                        'assets/icons/close_icon.png',
-                        scale: 3,
+            width: widthScreen,
+            height: heightScreen,
+            color: Colors.white.withOpacity(0.7),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 0.066 * heightScreen,
+                  ),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.transparent,
+                        shadowColor: Colors.transparent),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Image.asset(
+                      'assets/icons/close_icon.png',
+                      scale: 3,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: heightScreen * 0.18,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 25,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.author,
+                        style: const TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'CeraPro',
+                        ),
                       ),
-                    ),
+                      SizedBox(
+                        height: heightScreen * 0.03,
+                      ),
+                      Text(
+                        widget.content,
+                        style: const TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.black,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'CeraPro',
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    height: heightScreen * 0.3,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 0.03 * heightScreen),
-                    child: Column(
-                      children: [
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            widget.author,
-                            textAlign: TextAlign.start,
-                            style: const TextStyle(
-                                decoration: TextDecoration.none,
-                                color: Colors.black,
-                                fontSize: 22,
-                                fontWeight: FontWeight.w500,
-                                fontFamily: 'CeraPro'),
-                          ),
-                        ),
-                        SizedBox(
-                          height: heightScreen * 0.03,
-                        ),
-                        Text(
-                          widget.content,
-                          style: const TextStyle(
-                              decoration: TextDecoration.none,
-                              color: Colors.black,
-                              fontSize: 22,
-                              fontWeight: FontWeight.w500,
-                              fontFamily: 'CeraPro'),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              )),
+                ),
+              ],
+            ),
+          ),
         );
       });
 }
